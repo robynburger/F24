46522bb03521704328534a83313cd9ba807f3aa7
robynburger <robynlburger@gmail.com>
2024-09-26 15:19:29 -0400

    Create populate_D

--- LTS.py
+++ LTS.py
@@ -1,6 +1,11 @@
 import os
 import numpy as np
+'''
+Usage: 
+Change "string" below, then in command line:
+$ python LTS.py
 
+'''
 string = "BABBCA"
 
 '''
@@ -11,7 +16,7 @@ def populate_F(F, s):
   for k in range(1, n+1):
     for i in range (1, k+1): 
       for j in range(i+1, k+1):
-            print(f"\nk: {k}. i: {i}, j: {j}")
+            # print(f"\nk: {k}. i: {i}, j: {j}")
             if s[i-1] == s[k-1]: 
               # print(f"i-1 = {i-1}, s[i-1] = {s[i-1]}")
               # print(f"k-1 = {k-1}, s[i-1] = {s[i-1]}")
@@ -24,19 +29,38 @@ def populate_F(F, s):
               F[k][i][j]= max(F[k][i-1][j],F[k-1][i][j])
   return F
 
+'''
+# Populates the k-dimensional matrix D such that D[k][i][j] = D_k(i, j)
+'''
+def populate_D(D, F, n):
+  D = np.zeros((n, n, n), dtype=int)
+  for k in range(1, n+1):
+    for i in range (1, n+1):
+     for j in range(i+1, n+1):
+        D[k-1][i-1][j-1] = F[k][i][j] - F[k][i-1][j]
+  return D
 
+'''
+Prints F and D for input string 's'
+'''
 def LTS(s):
   n = len(s)
   # Pyton allows you to access the -1^st index, so initially, the F matrices have a buffer row and column of 0s
   empty_bufferF = np.zeros((n+1, n+1, n+1), dtype=int)
   bufferF = populate_F(empty_bufferF, s)
-  F = np.zeros((n, n, n), dtype=int)
 
-  # remove buffer row/column 
-  for dim in range(1, n+1):
-    for row in range (1, n+1):
-     for col in range(1, n+1):
-        F[dim-1][row-1][col-1] = bufferF[dim][row][col]
-  return F
+  D = populate_D(np.zeros((n, n, n), dtype=int), bufferF, n)
+
   
-print(LTS(string))
+  # remove F's buffer row/column 
+  F = np.zeros((n, n, n), dtype=int)
+  for k in range(1, n+1):
+    for i in range (1, n+1):
+     for j in range(1, n+1):
+        F[k-1][i-1][j-1] = bufferF[k][i][j]
+    print(f"F{k}: \n {F[k-1]}")
+    print(f"D{k}: \n {D[k-1]} \n")
+  
+  return 0
+  
+LTS(string)
